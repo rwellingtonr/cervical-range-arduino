6cd6e84ab3e4eb38f156a1548d9138aade10ab95
Wellington Leardini <wellington-158@hotmail.com>
2022-08-07 12:35:41 -0300

    desenvolvimento da lágica serial

--- Cervical_range/Cervical_range.ino
+++ Cervical_range/Cervical_range.ino
@@ -1,22 +1,29 @@
+// Bibliotecas
 
 #include <MPU6050_tockn.h>
 #include <Wire.h>
-// Leds
+// LEDS
 #define pinLedRed 12
 #define pinLedYellow 11
 #define pinLedGreen 10
 
-// valor do ângulo, alterar para o eixo a ser utilizado
-float initalAngleValue, angleY = 0;
+// Pre loads
+float angleY = 0;
 float sensorFlex = 0;
+char sessionStatus;
+const int updateTime = 500;
+
+unsigned long timeControl;
 
 MPU6050 mpu6050(Wire);
 
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(300);
   Wire.begin();
   mpu6050.begin();
-  mpu6050.calcGyroOffsets(false); // remove logs de calibração
+  // Log do estado inicial do sendor
+  mpu6050.calcGyroOffsets(true); 
   
   // define pinos de saída
   pinMode(pinLedRed, OUTPUT);
@@ -26,42 +33,50 @@ void setup() {
 
   // Pisca led para demonstrar final da calibração
   // Envia evento para informar o final da calibração
-  tare(1500);
-  // defini valor inicial do sensor giroscópio
-  setIntialValue();
+  tare();
+  
 }
 
 void loop() {
   mpu6050.update();
   angleY = mpu6050.getAngleY()
+  if(Serial.available()){
+    sessionStatus = Serial.readString();
 
+      // Se o valor inicial é maior que zero
+      // começa a processar os dados
+    if(sessionStatus == "started"){
+
+      // envia os dados a cada 500ms
+      if(millis() - timeControl > updateTime){
+          emmiter(angleY);
+          timeControl = millis();
+        }
+    }
+
+    if(sessionStatus == "stoped"){
+
+    }
+    
+  }
   /* TO DO
    [] Se menor/maior (TBD) finaliza as medições
    [] Ler o valor das coletas do sensor flexível
    [] Criar função aritimética do valor coletado
-   [] Envia a cada 200ms o valor coletado do sensor flexível
+   [x] Envia a cada 500ms o valor coletado do sensor flexível
   */
-  
+
 }
 
-void tare(int time){
+void tare(){
+  blinkLED(1500); // 1,5s
+  emmiter("tare");
+}
+
+void blinkLED(int time){
   ledsON();
   delay(time);
   ledsOFF()
-  emmiter("tare");
- }
-
-void emmiter(char event){
-  Serial.print(event);
-}
-void setIntialValue(){
-    mpu6050.update();
-    initalAngleValue = mpu6050.getAngleY();
-} 
-
-float calculateSensorFlex(){
-  // aqui vai alguma função matemática
-  return 3.1415;
 }
 void ledsON(){
   digitalWrite(pinLedRed,HIGH);
@@ -72,4 +87,14 @@ void ledsOFF(){
   digitalWrite(pinLedRed,LOW);
   digitalWrite(pinLedYellow,HIGH);
   digitalWrite(pinLedGreen,LOW);
+}
+
+void emmiter(float payload){
+  Serial.print("Value: ");
+  Serial.println(payload);
+}
+ 
+float calculateSensorFlex(){
+  // aqui vai alguma função matemática
+  return 3.1415;
 }
